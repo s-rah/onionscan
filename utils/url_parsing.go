cd9bfa2d9fd39c60861aa36a421700177910d094
Nick Doiron <ndoiron@mapmeld.com>
2016-05-26 16:44:55 +0700

    Scan CSS style tags and stylesheet links

--- utils/url_parsing.go
+++ utils/url_parsing.go
@@ -2,11 +2,23 @@ package utils
 
 import (
 	"github.com/mvdan/xurls"
+	"regexp"
 	"strings"
 )
 
 func ExtractDomains(content string) []string {
-	return xurls.Strict.FindAllString(content, -1)
+	domains := xurls.Strict.FindAllString(content, -1)
+	cssurlregex := regexp.MustCompile(`(?i)url\((.*?)\)`)
+	cssDomains := cssurlregex.FindAllString(content, -1)
+	for _, cssDomain := range cssDomains {
+		if strings.HasPrefix(strings.ToLower(cssDomain), "url(") {
+			cssDomain = cssDomain[4 : len(cssDomain)-1]
+		}
+		if !strings.HasSuffix(cssDomain, ":before") && !strings.HasSuffix(cssDomain, ":after") {
+			domains = append(domains, cssDomain)
+		}
+	}
+	return domains
 }
 
 func WithoutSubdomains(urlhost string) string {
@@ -24,6 +36,9 @@ func WithoutProtocol(url string) string {
 	}
 	if strings.HasPrefix(url, "https://") {
 		return url[8:]
+	}
+	if strings.HasPrefix(url, "//") {
+		return url[2:]
 	}
 	return url
 }
