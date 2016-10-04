f7fbd85bbe7b120a29ecf41fc2da0075ee312eb5
Wladimir J. van der Laan <laanwj@gmail.com>
2016-10-04 16:04:21 +0200

    Add test for deanonimization/check_exif
    Test image from rwcarlsen/goexif.

---
+++ deanonymization/check_exif_test.go
@@ -0,0 +1,102 @@
+package deanonymization
+
+import (
+	"fmt"
+	"github.com/s-rah/onionscan/report"
+	"github.com/s-rah/onionscan/utils"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+var truncated_jpeg = []byte{
+	0xff, 0xd8, 0xff, 0xe0, 0x00, 0x10, 0x4a, 0x46, 0x49, 0x46, 0x00, 0x01, 0x02, 0x01, 0x01, 0x00,
+	0x01, 0x00, 0x00, 0x00, 0xff, 0xe1, 0x14, 0x0a, 0x45, 0x78, 0x69, 0x66, 0x00, 0x00, 0x49, 0x49}
+
+var expected_exif = map[string]string{
+	"ColorSpace":              "65535",
+	"ComponentsConfiguration": "\"\"",
+	"DateTime":                "\"2012:11:04 05:42:02\"",
+	"ExifIFDPointer":          "134",
+	"ExifVersion":             "\"0210\"",
+	"FlashpixVersion":         "\"0100\"",
+	"Orientation":             "1",
+	"PixelXDimension":         "0",
+	"PixelYDimension":         "0",
+	"ResolutionUnit":          "2",
+	"XResolution":             "\"72/1\"",
+	"YCbCrPositioning":        "1",
+	"YResolution":             "\"72/1\"",
+}
+
+func TagsToMap(exiftags []report.ExifTag) map[string]string {
+	rv := make(map[string]string)
+	for _, tag := range exiftags {
+		rv[tag.Name] = tag.Value
+	}
+	return rv
+}
+
+func CompareMaps(t *testing.T, foundTags map[string]string, expectedTags map[string]string) {
+	var allTags []string
+	for k, _ := range expectedTags {
+		allTags = append(allTags, k)
+	}
+	for k, _ := range foundTags {
+		allTags = append(allTags, k)
+	}
+	utils.RemoveDuplicates(&allTags)
+
+	for _, tag := range allTags {
+		val1, ok1 := foundTags[tag]
+		val2, ok2 := expectedTags[tag]
+		if ok1 != ok2 {
+			if ok1 {
+				t.Error(fmt.Sprintf("Mismatch: tag %s found but not expected", tag))
+			} else {
+				t.Error(fmt.Sprintf("Mismatch: tag %s expected but not found", tag))
+			}
+		} else if val1 != val2 {
+			t.Error(fmt.Sprintf("Mismatch: tag %s found %s expected %s", tag, val1, val2))
+		}
+	}
+}
+
+func TestCheckExif(t *testing.T) {
+	ctx := CreateEnvContext(t)
+	defer ctx.Cleanup()
+
+	// Test 1: no EXIF images
+	CheckExif(ctx.osreport, ctx.report, ctx.osc)
+
+	if len(ctx.report.ExifImages) > 0 {
+		t.Error(fmt.Sprintf("Nothing crawled: Should not have detected EXIF"))
+	}
+
+	// Test 2: corrupted JPEG image
+	ctx.CreateBinaryPage("/test.jpg", 200, "image/jpeg", truncated_jpeg)
+
+	CheckExif(ctx.osreport, ctx.report, ctx.osc)
+
+	if len(ctx.report.ExifImages) > 0 {
+		t.Error(fmt.Sprintf("Corrupt image: Should not have detected EXIF"))
+	}
+
+	// Test 2: add real JPEG image
+	data, err := ioutil.ReadFile("testdata/f1-exif.jpg")
+	if err != nil {
+		t.Error(fmt.Sprintf("Error reading test image"))
+	}
+	ctx.CreateBinaryPage("/test2.jpg", 200, "image/jpeg", data)
+
+	CheckExif(ctx.osreport, ctx.report, ctx.osc)
+
+	if len(ctx.report.ExifImages) != 1 {
+		t.Error(fmt.Sprintf("Should have detected EXIF in test image"))
+	}
+	if !strings.HasSuffix(ctx.report.ExifImages[0].Location, "/test2.jpg") {
+		t.Error(fmt.Sprintf("Location on EXIF image doesn't match expected"))
+	}
+	var foundTags = TagsToMap(ctx.report.ExifImages[0].ExifTags)
+	CompareMaps(t, foundTags, expected_exif)
+}
--- deanonymization/private_key_test.go
+++ deanonymization/private_key_test.go
@@ -2,56 +2,8 @@ package deanonymization
 
 import (
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/crawldb"
-	"github.com/s-rah/onionscan/model"
-	"github.com/s-rah/onionscan/report"
-	"net/http"
-	"os"
 	"testing"
 )
-
-type EnvContext struct {
-	t        *testing.T
-	osreport *report.OnionScanReport
-	report   *report.AnonymityReport
-	osc      *config.OnionScanConfig
-	dbdir    string
-	onion    string
-}
-
-func CreateEnvContext(t *testing.T) *EnvContext {
-	ctx := new(EnvContext)
-	ctx.t = t
-	ctx.onion = "ktjts6vcmrumyy5x.onion"
-	ctx.dbdir = "/tmp/test-crawl"
-	os.RemoveAll(ctx.dbdir)
-
-	ctx.osreport = report.NewOnionScanReport(ctx.onion)
-	ctx.report = new(report.AnonymityReport)
-	ctx.osc = new(config.OnionScanConfig)
-	ctx.osc.Database = new(crawldb.CrawlDB)
-	ctx.osc.Database.NewDB(ctx.dbdir)
-
-	return ctx
-}
-
-func (ctx *EnvContext) CreatePage(filename string, status int, content_type string, content string) {
-	page := new(model.Page)
-	page.Status = status
-	page.Headers = make(http.Header)
-	page.Headers.Add("Content-Type", content_type)
-	page.Snapshot = content
-	id, err := ctx.osc.Database.InsertCrawlRecord("http://"+ctx.onion+filename, page)
-	if err != nil {
-		ctx.t.Error(fmt.Sprintf("Error inserting test document: %s", err))
-	}
-	ctx.osreport.Crawls[filename] = id
-}
-
-func (ctx *EnvContext) Cleanup() {
-	defer os.RemoveAll(ctx.dbdir)
-}
 
 func TestPrivateKey(t *testing.T) {
 	ctx := CreateEnvContext(t)
