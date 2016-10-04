976b6c3d492af82d76b1158aa521385b515d3102
Wladimir J. van der Laan <laanwj@gmail.com>
2016-10-04 21:16:51 +0200

    Add test for deanonimization/check_bitcoin_addresses

---
+++ deanonymization/check_bitcoin_addresses_test.go
@@ -0,0 +1,55 @@
+package deanonymization
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+func TestExtractBitcoinAddress(t *testing.T) {
+	ctx := CreateEnvContext(t)
+	defer ctx.Cleanup()
+
+	// Test 1
+	ExtractBitcoinAddress(ctx.osreport, ctx.report, ctx.osc)
+
+	if len(ctx.report.BitcoinAddresses) > 0 {
+		t.Error(fmt.Sprintf("Nothing crawled: Should not have detected a bitcoin address"))
+	}
+
+	// Test 2: P2PKH bitcoin address
+	ctx.CreatePage("/index.html", 200, "text/html", "<html><body>1CFCJHzSAC12VaJt2s1BwXgpb6beo9yWZU</body></html>")
+	ExtractBitcoinAddress(ctx.osreport, ctx.report, ctx.osc)
+
+	if len(ctx.report.BitcoinAddresses) != 1 {
+		t.Error(fmt.Sprintf("Should have detected a bitcoin address"))
+	}
+	if ctx.report.BitcoinAddresses[0] != "1CFCJHzSAC12VaJt2s1BwXgpb6beo9yWZU" {
+		t.Error(fmt.Sprintf("Unexpected bitcoin address found"))
+	}
+
+	// Test 3: P2SH (BIP13) bitcoin address
+	ctx.report.BitcoinAddresses = []string{}
+	ctx.CreatePage("/index.html", 200, "text/html", "<html><body>342ftSRCvFHfCeFFBuz4xwbeqnDw6BGUey</body></html>")
+	ExtractBitcoinAddress(ctx.osreport, ctx.report, ctx.osc)
+
+	if len(ctx.report.BitcoinAddresses) != 1 {
+		t.Error(fmt.Sprintf("Should have detected a bitcoin address"))
+	}
+	if ctx.report.BitcoinAddresses[0] != "342ftSRCvFHfCeFFBuz4xwbeqnDw6BGUey" {
+		t.Error(fmt.Sprintf("Unexpected bitcoin address found"))
+	}
+
+	// Test 4: Multiple addresses
+	ctx.report.BitcoinAddresses = []string{}
+	data, err := ioutil.ReadFile("testdata/bitcoin.html")
+	if err != nil {
+		t.Error(fmt.Sprintf("Error reading test html"))
+	}
+	ctx.CreatePage("/index.html", 200, "text/html", string(data))
+	ExtractBitcoinAddress(ctx.osreport, ctx.report, ctx.osc)
+
+	if len(ctx.report.BitcoinAddresses) != 100 {
+		t.Error(fmt.Sprintf("Should have detected 100 bitcoin addresses"))
+	}
+}
