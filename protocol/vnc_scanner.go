8333fe74677c4e70eb15a20750b30e7cda7f6b18
Sarah Jamie Lewis <me@sarahjamielewis.com>
2016-10-05 17:55:15 -0700

    Extract Desktop name and size from VNC Sessions

--- protocol/vnc_scanner.go
+++ protocol/vnc_scanner.go
@@ -2,12 +2,20 @@ package protocol
 
 import (
 	"fmt"
+	"github.com/mitchellh/go-vnc"
 	"github.com/s-rah/onionscan/config"
 	"github.com/s-rah/onionscan/report"
 	"github.com/s-rah/onionscan/utils"
 )
 
 type VNCProtocolScanner struct {
+}
+
+type VNCInfo struct {
+	DesktopName string
+	Width       uint16
+	Height      uint16
+	Error       string
 }
 
 func (vncps *VNCProtocolScanner) ScanProtocol(hiddenService string, osc *config.OnionScanConfig, report *report.OnionScanReport) {
@@ -19,8 +27,24 @@ func (vncps *VNCProtocolScanner) ScanProtocol(hiddenService string, osc *config.
 		report.VNCDetected = false
 	} else {
 		osc.LogInfo("Detected possible VNC instance\n")
-		// TODO: Actual Analysis
+
 		report.VNCDetected = true
+		config := new(vnc.ClientConfig)
+		ms := make(chan vnc.ServerMessage)
+		config.ServerMessageCh = ms
+		vc, err := vnc.Client(conn, config)
+		vncinfo := new(VNCInfo)
+		if err == nil {
+			osc.LogInfo(fmt.Sprintf("VNC Desktop Detected: %s %s (%v x %v)\n", hiddenService, vc.DesktopName, vc.FrameBufferWidth, vc.FrameBufferHeight))
+			vncinfo.DesktopName = vc.DesktopName
+			vncinfo.Width = vc.FrameBufferWidth
+			vncinfo.Height = vc.FrameBufferHeight
+		} else {
+			osc.LogError(err)
+			vncinfo.Error = err.Error()
+		}
+		report.AddProtocolInfo("vnc", 5900, vncinfo)
+
 	}
 	if conn != nil {
 		conn.Close()
