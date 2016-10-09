e6c5e095080529c20e06ce78e614fbc645b4fe8a
Sarah Jamie Lewis <me@sarahjamielewis.com>
2016-10-08 21:24:29 -0700

    Introduce Relationship Database
    This commit introduces the concept of storing identifier
    relationships in a seperate store from the raw crawl data.
    
    This will make it easy to built the Analytics Framework and
    identify correlations.
    
    The future of the database structure is now partially defined
    in the design/ folder, where new major design changes should
    be documented.

--- crawldb/crawldb_test.go
+++ crawldb/crawldb_test.go
@@ -28,3 +28,44 @@ func TestCrawlDB(t *testing.T) {
 		t.Errorf("Could not find crawl record in the database!")
 	}
 }
+
+func TestRelationship(t *testing.T) {
+
+	dbdir, err := ioutil.TempDir("", "test-crawl")
+	if err != nil {
+		t.Error(fmt.Sprintf("Error creating temporary directory: %s", err))
+	}
+	defer os.RemoveAll(dbdir)
+
+	db := new(CrawlDB)
+	db.NewDB(dbdir)
+	_, err = db.InsertRelationship("example.onion", "ssh", "12:23:32:DE:AD:BE:EF")
+	if err != nil {
+		t.Errorf("Relationship record was not stored in the database!")
+	}
+
+	_, err = db.InsertRelationship("example2.onion", "ssh", "12:23:32:DE:AD:BE:EF")
+	if err != nil {
+		t.Errorf("Relationship record was not stored in the database!")
+	}
+
+	result, _ := db.GetOnionsWithIdentifier("12:23:32:DE:AD:BE:EF")
+
+	if result == nil {
+		t.Errorf("Could not find relationships in the database!")
+	}
+
+	if len(result) != 2 {
+		t.Errorf("Relationships returned %d results, should return 2", len(result))
+	}
+
+	// DB could return results out of order.
+	if result[0] == "example.onion" && result[1] == "example2.onion" {
+		// OK
+	} else if result[1] == "example.onion" && result[2] == "example2.onion" {
+		// OK
+	} else {
+		t.Errorf("Relationships returned wrong onions %v", result)
+	}
+
+}
