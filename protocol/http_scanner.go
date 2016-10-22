dafdc82ef4995fadb58840aa8b81ed893e40ce38
Scott Ainslie <scott@scottainslie.me.uk>
2016-10-22 16:40:17 -0400

    Hypertext Transfer Protocol

--- protocol/http_scanner.go
+++ protocol/http_scanner.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/s-rah/onionscan/config"
 	"github.com/s-rah/onionscan/report"
-
 	"github.com/s-rah/onionscan/spider"
 	"github.com/s-rah/onionscan/utils"
 	"net/http"
@@ -27,20 +26,6 @@ func (hps *HTTPProtocolScanner) ScanProtocol(hiddenService string, osc *config.O
 		report.WebDetected = false
 	} else {
 		osc.LogInfo("Found potential service on http(80)\n")
-		report.WebDetected = true
-		wps := new(spider.OnionSpider)
-		wps.Crawl(report.HiddenService, osc, report)
-	}
-	osc.LogInfo(fmt.Sprintf("Checking %s http(8080)\n", hiddenService))
-	conn, err := utils.GetNetworkConnection(hiddenService, 8080, osc.TorProxyAddress, osc.Timeout)
-	if conn != nil {
-		conn.Close()
-	}
-	if err != nil {
-		osc.LogInfo("Failed to connect to service on port 8080\n")
-		report.WebDetected = false
-	} else {
-		osc.LogInfo("Found potential service on http(8080)\n")
 		report.WebDetected = true
 		wps := new(spider.OnionSpider)
 		wps.Crawl(report.HiddenService, osc, report)
