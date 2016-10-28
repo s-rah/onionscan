835babdf5859411b885413ec5ffbeabf969c982e
Sarah Jamie Lewis <me@sarahjamielewis.com>
2016-10-27 23:15:00 -0700

    Removing xurls Dependency
    Turns out are some point we dropped the need for it.

--- utils/url_parsing.go
+++ utils/url_parsing.go
@@ -1,25 +1,8 @@
 package utils
 
 import (
-	"github.com/mvdan/xurls"
-	"regexp"
 	"strings"
 )
-
-func ExtractDomains(content string) []string {
-	domains := xurls.Strict.FindAllString(content, -1)
-	cssurlregex := regexp.MustCompile(`(?i)url\((.*?)\)`)
-	cssDomains := cssurlregex.FindAllString(content, -1)
-	for _, cssDomain := range cssDomains {
-		if strings.HasPrefix(strings.ToLower(cssDomain), "url(") {
-			cssDomain = cssDomain[4 : len(cssDomain)-1]
-		}
-		if !strings.HasSuffix(cssDomain, ":before") && !strings.HasSuffix(cssDomain, ":after") {
-			domains = append(domains, cssDomain)
-		}
-	}
-	return domains
-}
 
 func WithoutSubdomains(urlhost string) string {
 	urlParts := strings.Split(urlhost, ".")
