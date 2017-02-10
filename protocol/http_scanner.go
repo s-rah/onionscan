443fe86641fde9a65219dcdfea128f260c1acba7
JosephGregg <JosephGregg@users.noreply.github.com>
2017-02-10 09:59:02 -0500

    update http ports
    Added range to allow for checking for port 8080 in addition to port 80

--- protocol/http_scanner.go
+++ protocol/http_scanner.go
@@ -1,34 +1,41 @@
 package protocol
 
 import (
-	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
+        "fmt"
+        "github.com/s-rah/onionscan/config"
+        "github.com/s-rah/onionscan/report"
 
-	"github.com/s-rah/onionscan/spider"
-	"github.com/s-rah/onionscan/utils"
-	"net/http"
+        "github.com/s-rah/onionscan/spider"
+        "github.com/s-rah/onionscan/utils"
+        "net/http"
 )
 
 type HTTPProtocolScanner struct {
-	Client *http.Client
+        Client *http.Client
 }
 
 func (hps *HTTPProtocolScanner) ScanProtocol(hiddenService string, osc *config.OnionScanConfig, report *report.OnionScanReport) {
 
-	// HTTP
-	osc.LogInfo(fmt.Sprintf("Checking %s http(80)\n", hiddenService))
-	conn, err := utils.GetNetworkConnection(hiddenService, 80, osc.TorProxyAddress, osc.Timeout)
-	if conn != nil {
-		conn.Close()
-	}
-	if err != nil {
-		osc.LogInfo("Failed to connect to service on port 80\n")
-		report.WebDetected = false
-	} else {
-		osc.LogInfo("Found potential service on http(80)\n")
-		report.WebDetected = true
-		wps := new(spider.OnionSpider)
-		wps.Crawl(report.HiddenService, osc, report)
-	}
+        // HTTP
+        ports := []int{80, 8080}
+
+        for _, port := range ports {
+                //fmt.Println(port)
+
+                osc.LogInfo(fmt.Sprintf("Checking %s http(%d)\n", hiddenService, port))
+                conn, err := utils.GetNetworkConnection(hiddenService, port, osc.TorProxyAddress, osc.Timeout)
+                if conn != nil {
+                        conn.Close()
+                }
+
+                if err != nil {
+                        osc.LogInfo(fmt.Sprintf("Failed to connect to service on port %d\n", port))
+                        report.WebDetected = false
+                } else {
+                        osc.LogInfo(fmt.Sprintf("Found potential service on http(%d)\n", port))
+                        report.WebDetected = true
+                        wps := new(spider.OnionSpider)
+                        wps.Crawl(report.HiddenService, osc, report)
+                }
+        }
 }
