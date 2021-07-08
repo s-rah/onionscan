44aa72d6971ba9c025ee1d26fadf27d3be7492c4
lovegreen1321 <87122618+lovegreen1321@users.noreply.github.com>
2021-07-08 23:20:49 +0530

    Update validation.go

--- utils/validation.go
+++ utils/validation.go
@@ -8,7 +8,7 @@ import (
 func IsOnion(identifier string) bool {
 	// TODO: At some point we will want to support i2p
 	if len(identifier) >= 22 && strings.HasSuffix(identifier, ".onion") {
-		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16}\.onion$`, identifier)
+		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16,56}\.onion$`, identifier)
 		return matched
 	}
 	return false
