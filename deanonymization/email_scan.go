5b9cf301559cfb699b38c7820b9b27d1b666e06c
csimsv <csimsv@gmail.com>
2022-11-30 14:59:46 -0600

    Update email_scan.go

--- deanonymization/email_scan.go
+++ deanonymization/email_scan.go
@@ -1,8 +1,8 @@
 package deanonymization
 
 import (
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
+	"github.com/csimsv/onionscan/config"
+	"github.com/csimsv/onionscan/report"
 	"regexp"
 	"strings"
 )
