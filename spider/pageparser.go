bb3e0d80d96b51df7c64d6d2e1ba9984ed71ca96
csimsv <csimsv@gmail.com>
2022-11-30 15:10:28 -0600

    Update pageparser.go

--- spider/pageparser.go
+++ spider/pageparser.go
@@ -2,8 +2,8 @@ package spider
 
 import (
 	"bytes"
-	"github.com/s-rah/onionscan/model"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/csimsv/onionscan/model"
+	"github.com/csimsv/onionscan/utils"
 	"golang.org/x/net/html"
 	"io"
 	"net/url"
