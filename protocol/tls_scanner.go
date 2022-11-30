68a0d1ab441c0508d6c2d22706fea21d871911bf
csimsv <csimsv@gmail.com>
2022-11-30 15:08:36 -0600

    Update tls_scanner.go

--- protocol/tls_scanner.go
+++ protocol/tls_scanner.go
@@ -3,9 +3,9 @@ package protocol
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/csimsv/onionscan/config"
+	"github.com/csimsv/onionscan/report"
+	"github.com/csimsv/onionscan/utils"
 )
 
 type TLSProtocolScanner struct {
