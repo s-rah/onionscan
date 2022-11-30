3ad0433dfc64e7526b590bb98010adf5b9094a90
csimsv <csimsv@gmail.com>
2022-11-30 15:02:50 -0600

    Update process_report.go

--- deanonymization/process_report.go
+++ deanonymization/process_report.go
@@ -1,9 +1,9 @@
 package deanonymization
 
 import (
-	"github.com/s-rah/onionscan/config"
-	"github.com/s-rah/onionscan/report"
-	"github.com/s-rah/onionscan/utils"
+	"github.com/csimsv/onionscan/config"
+	"github.com/csimsv/onionscan/report"
+	"github.com/csimsv/onionscan/utils"
 )
 
 // ExtractIdentifierStep is a pipeline step which extracts as many relationships
